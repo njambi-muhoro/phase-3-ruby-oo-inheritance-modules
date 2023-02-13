c1beb5e3cc7092aeb932766a7356c99dde06d186
+ <+>
2023-02-14 01:34:44 +0300

    Assignment Done

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,22 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative 'fancy_dance'
+
+class Dancer
+
+#Namespacing Modules - contains nested modules...look at meta_dancing_module.rb
+
+extend FancyDance::ClassMethods
+# extend means we are extending a class method from the module
+include FancyDance::InstanceMethods
+
+
+#normal module
+    include Dance
+  
+    attr_accessor :name
+  
+    def initialize(name)
+      @name = name
+    end
+  end
